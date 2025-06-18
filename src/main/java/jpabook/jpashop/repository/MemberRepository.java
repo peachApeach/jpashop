f3a3f64c9df2e8929e23de9d493f5a05133362eb
peachApeach <yh_01@daum.net>
2025-06-18 19:56:13 +0900

    ADD: member repository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);   //pk
+    }
+
+    public List<Member> findAll() {
+        //jpql : entity 객체를 대상으로 쿠리
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member where m.name = :name", Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
+}
